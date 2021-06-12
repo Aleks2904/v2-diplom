62b6b41950db8fe70e486d558116e6a62823ea6a
Aleks2904 <Garmatin171@yandex.ru>
2021-06-12 16:55:06 +0700

    v.9

--- app/js/bg-header-drop-list.js
+++ app/js/bg-header-drop-list.js
@@ -1,27 +1,25 @@
 document.addEventListener('DOMContentLoaded', function(){
     const headerChoise = document.querySelectorAll('.header-search-block__drop-item-container-list');
-        
-    for(let i = 0; i < headerChoise.length; i++){
-        let lists = headerChoise[i],
-            listsId = lists.id,
-            links = lists.querySelectorAll('.header-search-block__drop-item-link');
 
-        for(let k = 0; k < links.length ;k++){
-            let key = links[k],
-                keyTest = key.textContent.replace(/ +/g, ' ').trim(),
-                img = `../img/img/header-drop-list/${listsId}/${keyTest}.png`;
-
-            test(img, key)
-            //obg[k] = `'${key}'`;
-
-        }
-    }
-    
-    async function test(img, it){
+    async function bgSave(img, item){
         let response = await fetch(img);
         
         if(response.ok){
-            it.style.backgroundImage = `url("${img}")`;
+            item.style.backgroundImage = `url("${img}")`;
         }  
     }
+
+    headerChoise.forEach(el=>{
+        let lists = el,
+            listsId = lists.id,
+            links = lists.querySelectorAll('.header-search-block__drop-item-link');
+
+        links.forEach(li=>{
+            let key = li,
+            keyText = key.textContent.replace(/ +/g, ' ').trim(),
+            img = `../img/img/header-drop-list/${listsId}/${keyText}.png`;
+
+            bgSave(img, key)
+        })
+    })
 })
